16455eddc5353656a1c5edc1ad52708a2de87700
JMazur121 <mrsilviril@gmail.com>
2019-03-05 20:43:52 +0100

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	public static final String MAIN_VIEW_URL = "/views/MainView.fxml";
+	public static final String WINDOW_TITLE = "Kodowanie predykcyjne";
+
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(MAIN_VIEW_URL));
+		Parent root = loader.load();
+		primaryStage.setTitle(WINDOW_TITLE);
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
